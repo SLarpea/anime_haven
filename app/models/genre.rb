628cf50ccb5744285ff20f921a1c2c3814920d47
asulana <arnold.sulana@gmail.com>
2019-09-09 17:42:12 +0800

    change the association in model genre

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,5 +1,5 @@
 class Genre < ApplicationRecord
-  has_many :animes, dependent: :destroy
+  has_many :anime_genres, dependent: :destroy
   has_many :animes, through: :anime_genres, dependent: :destroy
   validates :genre_name, presence: true
   validates :content, presence: true
