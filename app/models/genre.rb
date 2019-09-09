f241ba27518efcb56ead5ffe2e781c27d3ba0449
asulana <arnold.sulana@gmail.com>
2019-09-09 18:33:17 +0800

    Only admin can delete a genre

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,4 +3,5 @@ class Genre < ApplicationRecord
   has_many :animes, through: :anime_genres, dependent: :destroy
   validates :genre_name, presence: true
   validates :content, presence: true
+  paginates_per 6
 end
